7622a0e09d828ad978273c48611aab078fccbf1f
Uwe Pachler <uwe.pachler@gmail.com>
2025-11-02 00:54:03 +0100

    fix warning in cogenitor-test

--- cogenitor-test/src/petstore_file.rs
+++ cogenitor-test/src/petstore_file.rs
@@ -2,6 +2,7 @@ include!(concat!(env!("OUT_DIR"), "/petstore.rs"));
 
 #[test]
 pub fn test_pet_present() {
+    #[allow(unused)]
     use generated_api::Pet;
 
     // this test does not fail; if it compiles it means that the Pet type is available
